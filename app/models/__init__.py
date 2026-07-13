b20484074488288d904a46b67712cc4cb1de3b11
Rafly Pratama Hudzaifah Al Syahbani <inondezionenera@gmail.com>
2026-07-13 15:09:41 +0700

    Delete app directory

--- app/models/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Registrasi seluruh model ORM agar terdaftar di metadata Base."""
-from app.models.user import ActivityLog, AIInferenceLog, User
-
-__all__ = ["User", "ActivityLog", "AIInferenceLog"]
